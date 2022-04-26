cc964f11794cd3f46ddb640b0f5bb021ddbbeb5f
Alexander Schroeter <me@alexanderschroeter.net>
2022-04-26 08:49:53 +0000

    Tried to specify which compiler and libraries to use without success

--- makefile
+++ makefile
@@ -4,6 +4,7 @@ default: pyexamples
 
 pyexamples: setup.py pyexamples.pyx $(LIB_DIR)/libexamples.a
 	python3 setup.py build_ext --inplace && rm -f pyexamples.c && rm -Rf build
+#	python3 setup.py build_ext --inplace -c /opt/hipSYCL/bin/syclcc-clang && rm -f pyexamples.c && rm -Rf build
 
 $(LIB_DIR)/libexamples.a:
 	make -C $(LIB_DIR) libexamples.a
--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+import os
+
+os.environ["CXX"] = "/opt/hipSYCL/bin/syclcc-clang --hipsycl-targets='hip:gfx900'"# -frtti"
+os.environ["CC"] = "/opt/hipSYCL/bin/syclcc-clang --hipsycl-targets='hip:gfx900'"# -frtti"
+os.environ['LDSHARED'] = "/opt/hipSYCL/bin/syclcc-clang --hipsycl-targets='hip:gfx900' -shared"
 
 examples_extension = Extension(
     name="pyexamples",
+    language="c++",
     sources=["pyexamples.pyx"],
-    libraries=["examples"],
-    library_dirs=["lib"],
-    include_dirs=["lib"]
+    libraries=["examples"], #, "cudart", "rt-backend-cuda","rt-backend-omp", "hipSYCL_clang"],
+    library_dirs=["lib",], # "/usr/local/cuda/lib64","/opt/hipSYCL/lib/hipSYCL","/opt/hipSYCL/lib"],
+    include_dirs=["lib",], # "/opt/hipSYCL/incude"],
+    extra_compile_args=[], # "-L/opt/hipSYCL/lib/hipSYCL", "-lrt-backend-cuda", "-L/opt/hipSYCL/lib"],
 )
 setup(
     name="pyexamples",
